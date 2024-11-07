47c5f1787281fb167e36954a31e346dcc2e9a75b
hp-MXL <1797953250@qq.com>
2024-11-07 15:00:23 +0800

    2024-11-7

--- 垃圾.cpp
+++ 垃圾.cpp
@@ -1,9 +1,2 @@
 
 // 查询报警项
-for(int i=0; i < m_worksetfile->m_alarm_array.size(); i++)
-{
-    if(m_worksetfile->m_alarm_array[i].first == QString::number(static_dataArray))
-    {
-        alarm_item = m_worksetfile->m_alarm_array[i].second;
-    }
-}
